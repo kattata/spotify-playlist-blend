938a41cf68611e547bd89a3686fdb13223a00f79
kattata <kasia.laniecka@gmail.com>
2024-10-21 23:13:33 +0200

    Add signout

--- components/global/TheHeader.vue
+++ components/global/TheHeader.vue
@@ -2,6 +2,11 @@
 import type { UserCookie } from '~/types';
 
 const user = useCookie<UserCookie>('user');
+
+function handleSignOut() {
+  user.value = undefined;
+  navigateTo('/auth');
+}
 </script>
 
 <template>
@@ -11,15 +16,25 @@ const user = useCookie<UserCookie>('user');
       <BaseLink class="navigation-item" to="/">Playlist Blend</BaseLink>
 
       <ul class="navigation-items-desktop">
-        <li v-if="user?.displayName" class="navigation-item profile">
-          <BaseImage v-if="user?.profileImage" :src="user.profileImage" width="30px" />
-          <div>{{ user.displayName }}</div>
+        <li v-if="user?.displayName" class="navigation-item navigation-item-profile">
+          <div class="profile">
+            <BaseImage v-if="user?.profileImage" :src="user.profileImage" width="30px" />
+            <div>{{ user.displayName }}</div>
+          </div>
+          <div class="profile-sign-out">
+            <BaseButton @click="handleSignOut">Sign out</BaseButton>
+          </div>
         </li>
       </ul>
 
       <ul class="navigation-items-mobile">
-        <li v-if="user?.profileImage" class="navigation-item profile">
-          <BaseImage v-if="user?.profileImage" :src="user.profileImage" width="30px" />
+        <li v-if="user?.profileImage" class="navigation-item navigation-item-profile">
+          <div class="profile">
+            <BaseImage v-if="user?.profileImage" :src="user.profileImage" width="30px" />
+          </div>
+          <div class="profile-sign-out">
+            <BaseButton @click="handleSignOut">Sign out</BaseButton>
+          </div>
         </li>
       </ul>
     </header>
@@ -40,6 +55,10 @@ header {
 
     li {
       list-style-type: none;
+
+      .navigation-item {
+        margin-top: -5px;
+      }
     }
   }
 
@@ -51,7 +70,7 @@ header {
     z-index: -1;
     position: absolute;
     left: 0 @(min-width: 1400px) calc((100vw - var(--container-width-m) + var(--container-padding)) / 2 * -1);
-    top: 50%;
+    top: 50px;
     width: 100vw;
   }
 
@@ -66,12 +85,29 @@ header {
   .navigation-item {
     background-color: var(--color-background);
     padding-inline: 16px;
+    position: relative;
+  }
+
+  .profile-sign-out {
+    display: none;
+    position: absolute;
+    right: 0;
+  }
+
+  .navigation-item-profile {
+    &:hover {
+      & > .profile-sign-out {
+        display: block;
+        margin-right: 12px;
+      }
+    }
   }
 
   .profile {
     display: flex;
     gap: 12px;
     align-items: center;
+    padding-bottom: 12px;
 
     img {
       border-radius: 50%;
